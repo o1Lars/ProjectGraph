4cd86c8c40a040ccb0ff99a546bc6ace9cba35ad
Chris <cvand15@student.sdu.dk>
2023-11-14 14:13:08 +0100

    some initial work. Far from complete

--- minimal-frustration-graph/main-graph.py
+++ minimal-frustration-graph/main-graph.py
@@ -6,6 +6,9 @@ Requirements
 ------------
 Package matplotlib https://matplotlib.org/ which can be installed via PIP.
 Package networkx https://networkx.org/ which can be installed via PIP.
+Package random
+Module visualiser_rndgraph.py   ### add file path
+Module graph.py from            ### add file path
 Python 3.7 or higher.
 
 
@@ -13,6 +16,17 @@ Notes
 -----
 This program is devoped as a group project as part of the exam DS830 Introduction to Programming fall 2023.
 """
+# import dependencies
+import graph.py as g
+import visualiser_rndgraph as vrg
+import random as random
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib as mlb
+import sys
+sys.path.append('/Users/chrisvandborg/ProjectGraph/minimal-frustration-graph/visualiser_rndgraph.py')
+
+
 # Create GUI for the user to operate the program
 
 # GUI Setup:
@@ -39,6 +53,59 @@ This program is devoped as a group project as part of the exam DS830 Introductio
 # - - If everything went OK -> proceed to update ELSE -> ask for new input
 # - Quit/exit program
 
+
+def add_edges_from_lines(lines: str) -> list[tuple]:
+    """Read lines, check if line represent an edge of a graph.
+    Return list of edges
+
+    ## implement tests
+    """
+    # Store edges in a list
+    edges_list = []
+
+    # Iterate through the lines and add edges to the graph
+    for line in lines:
+        # Ignore lines starting with #
+        if line.startswith('#'):
+            continue
+        # Split the line by comma
+        nodes = line.split(',')
+        # Check if both values are valid integers
+        if len(nodes) == 2 and nodes[0].strip().isdigit() and nodes[1].strip().isdigit():
+            # Convert nodes to integers and add the edge to the graph
+            u, v = map(int, nodes)
+            # Add the edge as a tuple to the edges_list
+            edges_list.append((u, v))
+        else:
+            print("Invalid input.")
+
+    return edges_list
+
+
+def create_graph_from_file(file_path: str) -> list[tuple]:
+    """Read a file, checks if its valid and return a list of edges for a graph
+
+    ## implement tests
+    """
+    # Open the text file in read mode
+    try:
+        with open(file_path, 'r') as file:
+            # Read lines from the file and remove whitespaces
+            lines = [line.strip() for line in file.readlines() if line.strip()]
+    # Handle errors
+    except FileNotFoundError:
+        print("Error: The file could not be found.")
+    except IOError:
+        print("There was an error reading from the file.")
+
+    # add edges from file to edges_list
+    graph_edges = add_edges_from_lines(lines)
+
+    return graph_edges
+
+
+print(create_graph_from_file('/Users/chrisvandborg/ProjectGraph/test_graph_1.txt'))
+
 # run simulation according to update protocol
 # - Iterate over graph list
 # - - Store local metric
@@ -49,6 +116,7 @@ This program is devoped as a group project as part of the exam DS830 Introductio
 # End of program, show final analysis graph of frustration
 
 # Allow user to run new simulation or quit program.
+
 
 # Import doctest module
 if __name__ == "__main__":
