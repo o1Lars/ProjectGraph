212f25a2c2aecaa42fc47917fc4bd8fadbb57ccc
chris-pc <cvand15@student.sdu.dk>
2023-11-15 13:13:43 +0100

    added import from GUI-module to main-graph. Gui can now be strated from main-graph module

--- minimal_frustration_graph/main-graph.py
+++ minimal_frustration_graph/main-graph.py
@@ -28,7 +28,7 @@ sys.path.append(os.path.dirname(current_dir))
 
 # Import modules
 from minimal_frustration_graph import visualiser_rndgraph as vrg
-
+from project_graph import GUI_Graph_Frustration as GUI
 import random as random
 
 
