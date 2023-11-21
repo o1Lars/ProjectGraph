aebb7d21ca95f4c1cc186bc307d66a809e5049de
Jah7600 <121032515+Jah7600@users.noreply.github.com>
2023-11-21 19:59:03 +0100

    Add files via upload

---
+++ montecarloupdate.py
@@ -0,0 +1,27 @@
+import random
+import math
+import roughwork2
+
+def monte_carlo(self) -> None:
+    """Visit each site of the graph and swap colour if the exponential of the local action > a random float between 0,1.
+    Parameters
+    ----------
+    graph : list
+        DESCRIPTION. # TODO
+
+    # add tests
+    """
+    vertices_list = self.vertices_list
+    vertices_frustration = self.vertices_frustration
+    val_map = self.val_map
+
+    # iterate over vertices in vertices_dictionary
+    for vertex in vertices_list:
+        # set local action for each vertex
+        local_action = vertices_frustration[vertex]
+        if local_action > 0:
+            current_color = val_map[vertex]  # set current color
+            random_number = random.uniform(0, 1)  # generate random float between 0 and 1
+            exp_local_action = math.exp(local_action)
+
+
