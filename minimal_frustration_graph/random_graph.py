78be2661f2ee91b66d38296f5fa83f95f4fbf0ec
chris-pc <cvand15@student.sdu.dk>
2023-11-28 22:35:56 +0100

    minor fixes

--- minimal_frustration_graph/random_graph.py
+++ minimal_frustration_graph/random_graph.py
@@ -3,6 +3,7 @@ import random
 def generate_random_graph(n, p=0.6):
     """Generates a random graph with the user input number of vertices."""
 
+    # Randomly assign connection between vertices
     graph = [[0 for _ in range(n)] for _ in range(n)]
 
     for i in range(n):
@@ -10,22 +11,7 @@ def generate_random_graph(n, p=0.6):
             if random.random() < p:
                 graph[i][j] = graph[j][i] = 1
 
-    # return graph
-
-    """ valid_input = False
-
-    while not valid_input:
-        num_vertices = int(input("Enter a desired number of vertices between 2 and 75 for your random graph: "))
-        probability = 0.6
-
-        if num_vertices < 2 or num_vertices > 75:
-            print("The number of vertices must be between 2 and 75.")
-        else:
-            valid_input = True """
-
-    # random_graph = generate_random_graph(n=num_vertices, p=probability)
-
-
+    # Create list of edges
     edges = []
 
     for i in range(len(graph)):
@@ -34,7 +20,3 @@ def generate_random_graph(n, p=0.6):
                 edges.append((i, j))
 
     return edges
-    """ edges_random_graph = connections
-
-    print(edges_random_graph) """
-print(generate_random_graph(10, 0.6))
